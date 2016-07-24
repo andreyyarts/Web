949ebb12b501033a7722d9915ef0e2a318d89efc
andrey <andrey_7l@list.ru>
2016-07-25 03:10:32 +0500

    [+] added dump user table

--- Kindergarten/sql/13.07.2016.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `kindergarten`.`children` 
-ADD PRIMARY KEY (`Id_ch`),
-CHANGE COLUMN `Id_ch` `Id_ch` INT(11) NOT NULL AUTO_INCREMENT 
-CHANGE COLUMN `fio_ch` `fio_ch` VARCHAR(255) NOT NULL ,
-CHANGE COLUMN `sex_ch` `sex_ch` VARCHAR(1) NOT NULL ,
-CHANGE COLUMN `address_ch` `address_ch` TEXT NOT NULL ;
-
-ALTER TABLE `kindergarten`.`tarifs` 
-CHANGE COLUMN `descr_t` `descr_t` TEXT NOT NULL ;
--- Kindergarten/sql/18,07,2016.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `kindergarten`.`children` 
-ADD COLUMN `birthday_ch` DATE NOT NULL AFTER `health_ch`,
-ADD COLUMN `enrollment_date_ch` DATE NULL AFTER `birthday_ch`,
-ADD COLUMN `outlet_date_ch` DATE NULL AFTER `enrollment_date_ch`;
